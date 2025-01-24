7df5fc47b4666f6ce7d0cdbb261afdcc5f984358
Sowmith Gachula <sowmithgachula@Sowmiths-MacBook-Pro.local>
2025-01-24 15:35:39 +0530

    CRT

--- CRTDay4/src/PrimeFunction.java
+++ CRTDay4/src/PrimeFunction.java
@@ -3,33 +3,39 @@ import java.util.Scanner;
 public class PrimeFunction {
 	
 	
-	static int primeInBetween(int a, int b) {
-		int c=0,i=0;
-		for ( i=a;i<=b;i++) {
-			
-		   for(int k=1;k>=i;k++) {
-			   if(k%i==0) {
-				   c++;
-			   }if(c==2) {
-				   System.out.println(i);
-			   }
-		   }
-		}
-		return 0;
+    static int primeInBetween(int a, int b) {
+       int c=0;
+       int i = 0;
+    	for(int x=a;x<=b;x++) {
+    		int num = x;
+    	for(i=1;i<=num;i++) {
+        	if(num%i==0){
+        		c++;
+        	}
+    	}
+    	}
+    	if(c==2){
+    		System.out.println();
+    	}
 		
-	}
-	
-
-	public static void main(String[] args) {
+    	
+    	
+    	return 0;
+    	
+    }
+	 public static void main(String[] args) {
 		
 		int x,y;
 		Scanner sc = new Scanner(System.in);
-		Scanner sc1 = new Scanner(System.in);
 		System.out.println("Enter the range");
-		x = sc.nextInt();
-		y = sc1.nextInt();
-		primeInBetween(x,y);
- 
-	}
-
+		x=sc.nextInt();
+		Scanner sc1 = new Scanner(System.in);
+        y=sc1.nextInt();
+        primeInBetween(x,y);
+        
+        
 }
+	
+	}
+	
+
